d69feabe1ff551852c5ccc8c610728a31a93ed1b
Emin <eminskiper99@gmail.com>
2019-05-28 21:38:01 +0300

    set constraint to test drive tab

--- app/TestDrive.php
+++ app/TestDrive.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class TestDrive extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'model_id',
+        'user_id',
+        'test_drive_date'
+    ];
+    protected $table = 'test_drives';
+
 }
--- database/migrations/2019_05_28_165619_create_test_drives_table.php
+++ database/migrations/2019_05_28_165619_create_test_drives_table.php
@@ -15,6 +15,20 @@ class CreateTestDrivesTable extends Migration
     {
         Schema::create('test_drives', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('model_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->dateTime('test_drive_date');
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+
+            $table->foreign('model_id')
+            ->references('id')
+            ->on('car_models')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
             $table->timestamps();
         });
     }
